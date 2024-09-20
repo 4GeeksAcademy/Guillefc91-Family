81e9883d3ea9a633d15dd05bd00b68ee17b47d0b
Guillermo Fernández Coscollano <167807897+Guillefc91@users.noreply.github.com>
2024-09-20 19:26:48 +0000

    finish

--- src/app.py
+++ src/app.py
@@ -21,18 +21,49 @@ def handle_invalid_usage(error):
 
 
 # Generate sitemap with all your endpoints
-@app.route('/')
+@app.route('/', methods=['GET'])
 def sitemap():
     return generate_sitemap(app)
 
 
-@app.route('/members', methods=['GET'])
+@app.route('/members', methods=['GET', 'POST'])
 def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    response_body = {}
+    if request.method == 'GET':
+        members = jackson_family.get_all_members()
+        response_body ['message'] = "Listado de miembros de la familia"
+        response_body['results'] = members
+        return jsonify(response_body), 200
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        jackson_family.add_member(data)
+        response_body ['message'] = "Agregar un familiar"
+        response_body['results'] = jackson_family.get_all_members()
+
+        return jsonify(response_body), 200
+
+
+@app.route('/members/<int:member_id>', methods=['GET','PUT','DELETE'])     
+def member(member_id):
+    response_body = {}
+    member = jackson_family.get_all_members(member_id)
+    if request.method == 'GET':
+        #member = jackson_family.get_all_members(member_id)
+        jackson_family.get_member(member_id)
+        response_body['message'] = "Este es el usuario desde el GET"
+        response_body['results'] = member[0]
+        return response_body, 200
+    if request.method == 'PUT':
+        response_body['message'] = "Este es el usuario desde el PUT"
+        response_body['results'] = {}
+        return response_body, 200
+    if request.method == 'DELETE':
+
+        jackson_family.delete_member(member_id)
+        response_body['message'] = "Este es el usuario desde el DELETE"
+        response_body['results'] = jackson_family.get_all_members()
+        return response_body, 200
 
 
 # This only runs if `$ python src/app.py` is executed
